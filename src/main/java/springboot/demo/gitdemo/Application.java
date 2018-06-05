3e6dc3a4584dac2f9edfc23cfff6996db0033fbe
sangrei <sangrei@163.com>
2018-06-05 15:57:32 +0800

    v0.1.1

---
+++ src/main/java/springboot/demo/gitdemo/Application.java
@@ -0,0 +1,32 @@
+package springboot.demo.gitdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Value("${user.name}") 
+    private String name;
+	
+	@Value("${user.email}") 
+    private String email;
+    
+	@RequestMapping("/")
+    public String index(){
+        return "gitdemo index.";
+    }
+	
+    @RequestMapping("/userinfo")
+    public String userinfo(){
+        return name+","+email;
+    }
+}
